de81bcc8f9fdb57f55a744f6bf443a8fe8eebbe0
IsabelaSchwade <isabelaschwade@gmail.com>
2024-09-07 09:49:22 -0300

    formulario de ordenação

--- index.php
+++ index.php
@@ -12,8 +12,18 @@ $resultadoLivros = $db->query($queryLivros);
 $queryAutores = "SELECT * FROM autor";
 $resultadoAutores = $db->query($queryAutores); 
 
-
 echo "<h1>Biblioteca</h1>";
 echo "<h2>Livros</h2>";
 
+echo <<<HTML
+<form method="get" action="">
+<label for="ordenar_por">Ordenar livro por:</label>
+<select name="ordenar_por" id="ordenar_por">
+    <option selected disabled value="">Selecione</option>
+    <option value="nome_livro">Nome do Livro</option>
+    <option value="autor_livro">Autor do livro </option>
+    <option value="data_lancamento">Data de lançamento do livro</option>
+</select>
+<input type="submit" value="Ordenar">
+HTML;
 ?>
