48f06667474b115b7075357a0b62b9329bd5cea2
IsabelaSchwade <isabelaschwade@gmail.com>
2024-09-07 09:46:11 -0300

    titulo
    titulo

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo"Teste"
+echo "<link rel='stylesheet' type='text/css' href='style.css'>"; 
+
+$db = new mysqli("localhost", "root", "", "biblioteca"); 
+
+$queryLivros = "SELECT livro.*, autor.nome_autor 
+                FROM livro 
+                JOIN autor ON livro.autor_livro = autor.id_autor";
+$resultadoLivros = $db->query($queryLivros); 
+
+
+$queryAutores = "SELECT * FROM autor";
+$resultadoAutores = $db->query($queryAutores); 
+
+
+echo "<h1>Biblioteca</h1>"; // título da página
+echo "<br>";
+echo "<h2>Livros</h2>";
+
 ?>
